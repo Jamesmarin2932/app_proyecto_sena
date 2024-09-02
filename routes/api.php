932b80ed4dec7e6e64637c63ddae7bc6643f04b9
jamesmarin2932 <jamesmarin827@gmail.com>
2024-09-01 19:15:57 -0500

    se crearon metodos, rutas y request, para el modelo factura del proyecto

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FacturaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/factura/getdata', [FacturaController::class, 'getdata']);
+Route::post('/factura/save', [FacturaController::class, 'save']);
+Route::put('/factura/update', [FacturaController::class, 'update']);
+Route::delete('/factura/delete', [FacturaController::class, 'delete']);
